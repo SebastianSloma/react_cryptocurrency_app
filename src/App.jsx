49ccc676000ff14aa315024e57dead0cfdcdc6cb
Sebastian <sebastiansloma@gmail.com>
2023-03-26 13:07:52 +0200

    created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
+import { Navbar } from './components';
+
+import './App.css'
 
 const App = () => {
 	return (
 		<div className='app'>
-      <div className="navbar">
-
-      </div>
-      <div className="main">
-
-      </div>
-      <div className="footer">
-
-      </div>
-			
+			<div className='navbar'>
+				<Navbar />
+			</div>
+			<div className='main'>
+            <h1>Main section</h1>
+         </div>
+			<div className='footer'></div>
 		</div>
 	);
 };
